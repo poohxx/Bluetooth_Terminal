cb0f87e1809f3825d9a7dc87121213404716b3f1
Ayrat <poohxx84@gmail.com>
2021-12-24 06:23:04 +0300

    add rcView(ListAdapter)

---
+++ app/src/main/java/com/poohxx/bluetooth_terminal/ListItem.kt
@@ -0,0 +1,6 @@
+package com.poohxx.bluetooth_terminal
+
+data class ListItem(
+    val name : String,
+    val mac : String
+)
--- app/src/main/java/com/poohxx/bluetooth_terminal/MainActivity.kt
+++ app/src/main/java/com/poohxx/bluetooth_terminal/MainActivity.kt
@@ -6,26 +6,36 @@ import android.bluetooth.BluetoothManager
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.poohxx.bluetooth_terminal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var btAdapter: BluetoothAdapter? = null
-
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: RcViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         init()
     }
 
     private fun init(){
         val btManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         btAdapter = btManager.adapter
-
+        adapter = RcViewAdapter()
+        binding.rcView.layoutManager = LinearLayoutManager(this)
+        binding.rcView.adapter = adapter
+        getPairedDevices()
     }
+
     private fun getPairedDevices(){
         val pairedDevices: Set<BluetoothDevice>? = btAdapter?.bondedDevices
+        val tempList = ArrayList<ListItem>()
         pairedDevices?.forEach{
-            
+            tempList.add(ListItem(it.name, it.address))
         }
+        adapter.submitList(tempList)
     }
 }
